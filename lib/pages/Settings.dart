e231a4e19538dc41a9669523d8a5580abf387bfd
krshn-ptl <58908961+krshn-ptl@users.noreply.github.com>
2021-04-22 19:43:52 +0530

    permission changes

--- lib/pages/Settings.dart
+++ lib/pages/Settings.dart
@@ -7,6 +7,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'login.dart';
 
@@ -125,7 +126,7 @@ class _SettingPageState extends State<SettingPage> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Padding(
-                            padding: const EdgeInsets.only(top:8.0),
+                            padding: const EdgeInsets.only(top: 8.0),
                             child: FaIcon(
                               FontAwesomeIcons.phoneAlt,
                               size: 22,
@@ -134,31 +135,57 @@ class _SettingPageState extends State<SettingPage> {
                           Expanded(
                               child: Padding(
                             padding: const EdgeInsets.only(left: 20),
-                                child: Column(
-                                  //mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      'Dial Emergency numbers',
-                                      style: TextStyle(
-                                          fontSize: 22, fontWeight: FontWeight.w500),
-                                    ),
-                                    Text('Emergency Button will dial emergency number too along with sending sms to peers',
-                                      style: TextStyle(
-                                          fontSize: 14,color: Colors.grey, fontWeight: FontWeight.w500),)
-                                  ],
+                            child: Column(
+                              //mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Dial Emergency numbers',
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w500),
                                 ),
-
+                                Text(
+                                  'Emergency Call to 100 when Detected along with messages to your emergency Contacts',
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.w500),
+                                )
+                              ],
+                            ),
                           )),
                           Transform.scale(
                             scale: 0.8,
                             child: CupertinoSwitch(
-                              onChanged: (value) {
-                                setState(() {
-                                  dialEmergencyNumbers = value;
-                                  pref.setBool(
-                                      'emergency', dialEmergencyNumbers);
-                                });
+                              onChanged: (value) async {
+                                if (await Permission.phone.status.isDenied) {
+                                  await Permission.phone.request();
+                                }
+                                if (await Permission.phone.status.isGranted) {
+                                  setState(() {
+                                    dialEmergencyNumbers = value;
+                                    pref.setBool(
+                                        'emergency', dialEmergencyNumbers);
+                                  });
+                                } else {
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    content: Text(
+                                      "Permission Denied",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Theme.of(context).buttonColor),
+                                    ),
+                                    backgroundColor:
+                                        Theme.of(context).dividerColor,
+                                    elevation: 2,
+                                    duration: const Duration(seconds: 3),
+                                  ));
+                                  setState(() {
+                                    value = false;
+                                  });
+                                }
                               },
                               value: dialEmergencyNumbers ?? false,
                               activeColor: Colors.red,
@@ -173,7 +200,7 @@ class _SettingPageState extends State<SettingPage> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Padding(
-                            padding: const EdgeInsets.only(top:8.0),
+                            padding: const EdgeInsets.only(top: 8.0),
                             child: FaIcon(
                               FontAwesomeIcons.microphone,
                               size: 22,
@@ -182,7 +209,6 @@ class _SettingPageState extends State<SettingPage> {
                           Expanded(
                               child: Padding(
                             padding: const EdgeInsets.only(left: 26),
-
                             child: Column(
                               //mainAxisAlignment: MainAxisAlignment.start,
                               crossAxisAlignment: CrossAxisAlignment.start,
@@ -190,22 +216,74 @@ class _SettingPageState extends State<SettingPage> {
                                 Text(
                                   'Record Audio',
                                   style: TextStyle(
-                                      fontSize: 22, fontWeight: FontWeight.w500),
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w500),
                                 ),
-                                Text('Audio will be recorded in Alone Mode for further use if option is enabled',
+                                Text(
+                                  'Audio will be recorded in Alone Mode for further use if option is enabled',
                                   style: TextStyle(
-                                      fontSize: 14,color: Colors.grey, fontWeight: FontWeight.w500),)
+                                      fontSize: 14,
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.w500),
+                                )
                               ],
                             ),
                           )),
                           Transform.scale(
                             scale: 0.8,
                             child: CupertinoSwitch(
-                              onChanged: (value) {
-                                setState(() {
-                                  recordAudio = value;
-                                  pref.setBool('audio', recordAudio);
-                                });
+                              onChanged: (value) async {
+                                if (!phraseDetection) {
+                                  if (await Permission
+                                          .microphone.status.isDenied ||
+                                      await Permission.speech.isDenied) {
+                                    await Permission.microphone.request();
+                                    await Permission.speech.request();
+                                  }
+                                  if (await Permission.microphone.isGranted &&
+                                      await Permission.speech.isGranted) {
+                                    setState(() {
+                                      recordAudio = value;
+                                      pref.setBool('audio', recordAudio);
+                                    });
+                                  } else {
+                                    ScaffoldMessenger.of(context)
+                                        .showSnackBar(SnackBar(
+                                      content: Text(
+                                        "Permission Denied",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            color:
+                                                Theme.of(context).buttonColor),
+                                      ),
+                                      backgroundColor:
+                                          Theme.of(context).dividerColor,
+                                      elevation: 2,
+                                      duration: const Duration(seconds: 3),
+                                    ));
+                                    setState(() {
+                                      recordAudio = false;
+                                      value = false;
+                                    });
+                                  }
+                                } else {
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    content: Text(
+                                      "You cannot select Audio recording and phrase detection simultaneously",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Theme.of(context).buttonColor),
+                                    ),
+                                    backgroundColor:
+                                        Theme.of(context).dividerColor,
+                                    elevation: 2,
+                                    duration: const Duration(seconds: 3),
+                                  ));
+                                  setState(() {
+                                    value = false;
+                                  });
+                                }
                               },
                               value: recordAudio ?? false,
                               activeColor: Colors.red,
@@ -220,7 +298,7 @@ class _SettingPageState extends State<SettingPage> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Padding(
-                            padding: const EdgeInsets.only(top:8.0),
+                            padding: const EdgeInsets.only(top: 8.0),
                             child: FaIcon(
                               FontAwesomeIcons.dotCircle,
                               size: 24,
@@ -236,22 +314,74 @@ class _SettingPageState extends State<SettingPage> {
                                 Text(
                                   'Trigger Phrases Detection',
                                   style: TextStyle(
-                                      fontSize: 22, fontWeight: FontWeight.w500),
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w500),
                                 ),
-                                Text('If enabled, the app will try to detect Phrases like "Help" to trigger Emergency SMS',
+                                Text(
+                                  'If enabled, the app will try to detect Phrases like "Help" to trigger Emergency SMS',
                                   style: TextStyle(
-                                      fontSize: 14,color: Colors.grey, fontWeight: FontWeight.w500),)
+                                      fontSize: 14,
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.w500),
+                                )
                               ],
                             ),
                           )),
                           Transform.scale(
                             scale: 0.8,
                             child: CupertinoSwitch(
-                              onChanged: (value) {
-                                setState(() {
-                                  phraseDetection = value;
-                                  pref.setBool('phrase', phraseDetection);
-                                });
+                              onChanged: (value) async {
+                                if (!recordAudio) {
+                                  if (await Permission
+                                          .microphone.status.isDenied ||
+                                      await Permission.speech.isDenied) {
+                                    await Permission.microphone.request();
+                                    await Permission.speech.request();
+                                  }
+                                  if (await Permission.microphone.isGranted &&
+                                      await Permission.speech.isGranted) {
+                                    setState(() {
+                                      phraseDetection = value;
+                                      pref.setBool('phrase', phraseDetection);
+                                    });
+                                  } else {
+                                    ScaffoldMessenger.of(context)
+                                        .showSnackBar(SnackBar(
+                                      content: Text(
+                                        "You cannot select Audio recording and phrase detection simultaneously",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            color:
+                                                Theme.of(context).buttonColor),
+                                      ),
+                                      backgroundColor:
+                                          Theme.of(context).dividerColor,
+                                      elevation: 2,
+                                      duration: const Duration(seconds: 3),
+                                    ));
+                                    setState(() {
+                                      phraseDetection = false;
+                                      value = false;
+                                    });
+                                  }
+                                } else {
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    content: Text(
+                                      "You cannot select Audio recording and phrase detection simultaneously",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Theme.of(context).buttonColor),
+                                    ),
+                                    backgroundColor:
+                                        Theme.of(context).dividerColor,
+                                    elevation: 2,
+                                    duration: const Duration(seconds: 3),
+                                  ));
+                                  setState(() {
+                                    value = false;
+                                  });
+                                }
                               },
                               value: phraseDetection ?? false,
                               activeColor: Colors.red,
@@ -272,7 +402,7 @@ class _SettingPageState extends State<SettingPage> {
                           Expanded(
                               child: Padding(
                             padding: const EdgeInsets.only(left: 20),
-                            child:  Text(
+                            child: Text(
                               'Log Out',
                               style: TextStyle(
                                   fontSize: 22, fontWeight: FontWeight.w500),
